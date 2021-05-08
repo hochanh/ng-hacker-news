a340369d226a3df777487dbef4783837461142f3
manhtai <vo@manhtai.com>
2021-05-08 17:44:02 +0700

    Done

--- src/app/components/list-item/list-item.component.ts
+++ src/app/components/list-item/list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Output, Input, EventEmitter } from '@angular/core';
 
 import { ListItem } from '../../models/list-item';
 
@@ -8,5 +8,10 @@ import { ListItem } from '../../models/list-item';
   styleUrls: ['./list-item.component.scss']
 })
 export class ListItemComponent {
-  @Input() item: ListItem | undefined;
+  @Input() item: ListItem;
+  @Output() showDetail = new EventEmitter<ListItem>();
+
+  openDetail(item: ListItem): void {
+    this.showDetail.next(item);
+  }
 }
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -10,8 +10,10 @@ import { ListItem } from '../../models/list-item';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-  listItems: Observable<ListItem[]> | undefined;
+  listItems: Observable<ListItem[]>;
   apiService: ApiService;
+  showingDetail = false;
+  item: ListItem | null;
 
   constructor(
     apiService: ApiService,
@@ -21,5 +23,15 @@ export class ListComponent implements OnInit {
 
   ngOnInit(): void {
     this.listItems = this.apiService.getListItems();
+  }
+
+  showDetail(item: ListItem): void {
+    this.showingDetail = true;
+    this.item = item;
+  }
+
+  closeDetail(): void {
+    this.showingDetail = false;
+    this.item = null;
   }
 }
